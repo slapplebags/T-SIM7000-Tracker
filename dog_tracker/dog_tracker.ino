61e84fe2897c47ffb6a5b83c233e8102f6630f6f
slapplebags <richardnixonkingofthemoleppl@gmail.com>
2020-11-18 10:29:20 -0800

    now able to get dweet and run code based on json content.

--- dog_tracker/dog_tracker.ino
+++ dog_tracker/dog_tracker.ino
@@ -5,14 +5,17 @@
 #define FONA_TX 26 // ESP32 hardware serial RX2 (GPIO16)
 #define FONA_RX 27 // ESP32 hardware serial TX2 (GPIO17)
 #include <HardwareSerial.h>
+#include <ArduinoJson.h>
+
 HardwareSerial fonaSS(1);
 Adafruit_FONA_LTE fona = Adafruit_FONA_LTE();
 uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
 uint8_t type;
 char replybuffer[255]; // this is a large buffer for replies
 char imei[16] = {0}; // MUST use a 16 character buffer for IMEI!
-
+const char* found = "found";
 float latitude, longitude, speed_kph, heading, altitude, second;
+const char* url = "dweet.io/get/latest/dweet/for/869951031430170";
 
 void setup() {
   
@@ -57,6 +60,56 @@ fona.setFunctionality(1); // AT+CFUN=1
 }
 
 void loop() {
+
+        // read website URL
+        uint16_t statuscode;
+        int16_t length;
+        char url[80];
+        char replybuffer[254];
+        uint16_t replyidx = 0;
+        
+        flushSerial();
+
+        Serial.println(F("****"));
+        if (!fona.HTTP_GET_start(url, &statuscode, (uint16_t *)&length)) {
+          Serial.println("Failed!");
+
+        }
+        while (length > 0) {
+          while (fona.available()) {
+            char c = fona.read();
+            replybuffer[replyidx] = c;
+            replyidx++;
+            // Serial.write is too slow, we'll write directly to Serial register!
+            Serial.write(c);             
+          }
+        }
+        Serial.println(F("\n****"));
+        fona.HTTP_GET_end();
+        Serial.println(replybuffer);
+        
+        const size_t capacity = JSON_OBJECT_SIZE(1) + 2*JSON_OBJECT_SIZE(4) + 310;
+        DynamicJsonDocument doc(capacity);
+        const char* json = replybuffer;
+deserializeJson(doc, json);
+
+const char* by = doc["by"]; // "getting"
+const char* the = doc["the"]; // "dweets"
+
+JsonObject with_0 = doc["with"][0];
+const char* with_0_thing = with_0["thing"]; // "869951031430170"
+const char* with_0_created = with_0["created"]; // "2020-11-18T07:20:23.410Z"
+
+const char* with_0_content_state = with_0["content"]["state"]; // "found"
+        Serial.println(with_0_content_state);
+        if (strcmp(with_0_content_state, found) == 0){
+          Serial.println("fuck butt poops");
+        }
+      }
+
+
+
+void report() {
   delay(60000);
   //float latitude, longitude, speed_kph, heading, altitude, second;
         uint16_t year;
@@ -84,6 +137,13 @@ void loop() {
 
 }
 }
+
+
+void flushSerial() {
+  while (Serial.available())
+    Serial.read();
+}
+
 // Power on the module
 void powerOn() {
   digitalWrite(FONA_PWRKEY, LOW);
